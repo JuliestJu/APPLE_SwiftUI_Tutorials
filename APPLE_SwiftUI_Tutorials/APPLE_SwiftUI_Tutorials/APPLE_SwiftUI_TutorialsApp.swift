8b6ff840e2b63bf24beb14ca9dbafc23a23dbf35
Julia Vorotchenko <juliavorotchenko@gmail.com>
2023-06-26 19:23:02 +0300

    Add project

---
+++ APPLE_SwiftUI_Tutorials/APPLE_SwiftUI_Tutorials/APPLE_SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  APPLE_SwiftUI_TutorialsApp.swift
+//  APPLE_SwiftUI_Tutorials
+//
+//  Created by Юлія Воротченко on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct APPLE_SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
