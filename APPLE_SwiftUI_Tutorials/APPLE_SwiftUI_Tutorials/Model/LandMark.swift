4df842413d0dd6261f6dd50ce87062714593c11d
Julia Vorotchenko <juliavorotchenko@gmail.com>
2023-06-27 19:02:19 +0300

    Building Lists and Navigation, Section 1

---
+++ APPLE_SwiftUI_Tutorials/APPLE_SwiftUI_Tutorials/Model/LandMark.swift
@@ -0,0 +1,34 @@
+//
+//  LandMark.swift
+//  APPLE_SwiftUI_Tutorials
+//
+//  Created by Юлія Воротченко on 27.06.2023.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinates: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude,
+                               longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
