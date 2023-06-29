8e83c1e94eeb4d65ff6bb8b7225f546015dc07b2
Julia Vorotchenko <juliavorotchenko@gmail.com>
2023-06-29 18:56:11 +0300

    Section 2

---
+++ APPLE_SwiftUI_Tutorials/APPLE_SwiftUI_Tutorials/Views/LandMarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandMarkRow.swift
+//  APPLE_SwiftUI_Tutorials
+//
+//  Created by Юлія Воротченко on 29.06.2023.
+//
+
+import SwiftUI
+
+struct LandMarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandMarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandMarkRow(landmark: landmarks[0])
+    }
+}
