96a245827a4638baccfbb788f5f41ccc8f6fc256
Julia Vorotchenko <juliavorotchenko@gmail.com>
2023-07-03 19:54:24 +0300

    Add toggle Section3

--- APPLE_SwiftUI_Tutorials/APPLE_SwiftUI_Tutorials/Views/LandMarkRow.swift
+++ APPLE_SwiftUI_Tutorials/APPLE_SwiftUI_Tutorials/Views/LandMarkRow.swift
@@ -16,8 +16,12 @@ struct LandMarkRow: View {
                 .resizable()
                 .frame(width: 50, height: 50)
             Text(landmark.name)
-            
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
--- APPLE_SwiftUI_Tutorials/APPLE_SwiftUI_Tutorials/Views/LandmarksList.swift
+++ APPLE_SwiftUI_Tutorials/APPLE_SwiftUI_Tutorials/Views/LandmarksList.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 
 struct LandmarkList: View {
+    
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks
+            .filter { (!showFavoritesOnly || $0.isFavorite) }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landMark: landmark)
-                } label: {
-                    LandMarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landMark: landmark)
+                    } label: {
+                        LandMarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
